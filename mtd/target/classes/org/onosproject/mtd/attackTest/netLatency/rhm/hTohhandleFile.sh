677f7a126ba633bae07e9ff8047bd1983b7af29c
leibnizrz <1339905524@qq.com>
2024-05-13 15:42:59 +0800

    delete target

--- mtd/target/classes/org/onosproject/mtd/attackTest/netLatency/rhm/hTohhandleFile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-awk -F/ '/rtt/{print $5}' hostToHostNetLatency.log
-
-awk -F/ '/rtt/{i++;if(i<3){sum+=$5}} END { print sum*5}' hostToHostNetLatency.log
-awk -F/ '/rtt/{i++;if(i<5){sum+=$5}} END { print sum*5}' hostToHostNetLatency.log
-awk -F/ '/rtt/{i++;if(i<10){sum+=$5}} END { print sum*5}' hostToHostNetLatency.log
-
